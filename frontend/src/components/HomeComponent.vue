9bd270107d0040d5c724eb67e14c00b1d559a238
Hunter Schafer <hschafer@uw.edu>
2023-11-13 16:21:35 -0800

    Add basic multi-page structure

--- frontend/src/components/HomeComponent.vue
+++ frontend/src/components/HomeComponent.vue
@@ -1,8 +1,10 @@
-<script setup lang="ts">
+<script setup>
 import { ref } from 'vue'
 import axios from 'axios'
 
-defineProps<{ msg: string }>()
+defineProps({
+  msg: String
+})
 
 const count = ref(0)
 
@@ -15,6 +17,14 @@ axios.get("http://localhost/api/posts").then(response => {
 </script>
 
 <template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
   <h1>{{ msg }}</h1>
 
   <div class="card">
@@ -46,5 +56,18 @@ axios.get("http://localhost/api/posts").then(response => {
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createRouter, createWebHistory } from 'vue-router'
+
+import AboutComponent from './components/AboutComponent.vue'
+import App from './App.vue'
+import HomeComponent from './components/HomeComponent.vue'
+import { createApp } from 'vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: "/", component: HomeComponent},
+        { path: "/about", component: AboutComponent},
+    ]
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
