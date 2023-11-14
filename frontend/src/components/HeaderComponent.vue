9c7a4623b55e74a335356c0af92f569e08499d3c
Hunter Schafer <hschafer@uw.edu>
2023-11-13 17:17:34 -0800

    Minor changes to header styling

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div class="header">
-        <b>H</b> HunterNews
+        <div class="icon">H</div> <b>HunterNews</b>
     </div>
 </template>
 
@@ -14,5 +14,18 @@
         text-align: left;
         background-color: #F86602;
         padding: 0.5em 1em 0.5em 1em;
+    }
+
+    .icon {
+        display: inline-block;
+        width: 24px;
+        height: 24px;
+        padding: 0.1em;
+        text-align: center;
+        margin-right: 0.2em;
+        color: white;
+        border: white;
+        border-width: 1px;
+        border-style: solid;
     }
 </style>
