e4fee510a9ae4b34c884818d57c2445b868f896f
Hunter Schafer <hschafer@uw.edu>
2023-11-15 14:13:32 -0800

    Disable comment box when logged out

--- frontend/src/components/CommentBox.vue
+++ frontend/src/components/CommentBox.vue
@@ -16,28 +16,36 @@ const store = useStore();
 const comment = ref("");
 
 function addComment() {
-  axios.post("/api/comment/new", {
-    "comment": comment.value,
-    "postID": props.postID,
-    "parentID": props.parentID,
-  }, {
-    "headers": {
-      "Authorization": `Bearer ${store.getters.getToken}`
-    }
-  }).then(() => {
-    console.log("Comment posted");
-    comment.value = "";  // Clear the text box
-    props.onComment();
-  }).catch((error) => {
-    console.log("Error posting comment", error)
-  });
+  if (store.getters.isAuthenticated) {
+    axios.post("/api/comment/new", {
+      "comment": comment.value,
+      "postID": props.postID,
+      "parentID": props.parentID,
+    }, {
+      "headers": {
+        "Authorization": `Bearer ${store.getters.getToken}`
+      }
+    }).then(() => {
+      console.log("Comment posted");
+      comment.value = "";  // Clear the text box
+      props.onComment();
+    }).catch((error) => {
+      console.log("Error posting comment", error)
+    });
+  } else {
+    console.log("Need to be signed in to comment");
+  }
 }
 </script>
 
 <template>
     <div class="commentBox">
-      <textarea v-model="comment"></textarea>
-      <button @click="addComment">add comment</button>
+      <textarea
+        :disabled="!this.$store.getters.isAuthenticated"
+        v-model="comment"
+        :placeholder="this.$store.getters.isAuthenticated ? '' : 'Must be logged in to comment' ">
+      </textarea>
+      <button :disabled="!this.$store.getters.isAuthenticated" @click="addComment">add comment</button>
     </div>
 </template>
 
